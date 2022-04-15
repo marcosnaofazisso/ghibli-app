5b9b6e862dbddcaaf1de8ddb71150e8b6c80f939
Marcos Ferreira <marcosferreiraf22@gmail.com>
2022-04-15 13:45:48 -0300

    state mechanics added for studying

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Button, Alert } from "react-native";
 
 export default class Filmes extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nomeFilme: ""
+        }
+
+    }
+
+    verDetalhes = _ => {
+        Alert.alert("Voce escolheu o filme", this.props.data.title)
+        this.setState({ nomeFilme: this.props.data.title })
+
+    }
 
     render() {
         return (
@@ -11,9 +24,15 @@ export default class Filmes extends React.Component {
                     source={{
                         uri: this.props.data.image
                     }} />
+                    <Text>{this.state.nomeFilme}</Text>
                 <Text style={styles.title}>{this.props.index + 1} - {this.props.data.title}</Text>
                 <Text style={styles.subtitle}>{this.props.data.original_title}</Text>
                 <Text style={styles.description}>{this.props.data.description}</Text>
+                <Button
+                    title={"Ver detalhes"}
+                    color={"#0000FF"}
+                    onPress={this.verDetalhes.bind(this)}
+                />
             </View>)
     }
 }
